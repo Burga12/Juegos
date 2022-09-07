2624564c8e334b93b87064f9eb8264ae5d6c4d0f
Ronald <ronaldherpc@gmail.com>
2022-09-07 01:15:31 -0400

    Tenias unos bugs mi brother

--- ahorcado.cpp
+++ ahorcado.cpp
@@ -3,13 +3,13 @@
 #include <string.h>
 #include <stdio.h>
 #include <time.h>
-#define cls system("cls");
+#define cls system("clear");
 
 using namespace std;
 
 	const string words[5] = {"cambur","anzoategui","marico triste","hitler","akara messi"};
-	string help = "",palabra = "";
-	char key[100],letrasUsadas[100];
+	string help = "",palabra = "", letrasUsadas;;
+	char key[100];
 	int life = 10,bien = 0,cont = 0;
 	bool repetido = false;
 
@@ -24,7 +24,7 @@ int main(int argc, char const *argv[])
 	system("color 05");
 
 	srand(time(NULL));
-	int select = 0+rand()%(5-0);
+	int select = rand()%5;
 
 	palabra = words[select];
 
@@ -37,24 +37,26 @@ int main(int argc, char const *argv[])
 
 
 	cls;
-	if (life == 0) cout << "\n\n\t\t\tO vaya es una lastima has perdido";
+	if (life == 0) 
+		cout << "\n\n\t\t\tOh vaya! es una lastima que hayas perdido";
 	else {
-		cout << "\n\n O vaya. HAS GANADO, felicidades. \n\n Tu premio sera una recarga de saldo";
-		cout << "\n\n\tPalabra: ";for (int i = 0; i< palabra.length();i++) printf("%c",help[i]);
+		cout << "\n\n Oh! vaya. HAS GANADO, felicidades. \n\n Tu premio sera una recarga de saldo";
+		cout << "\n\n\tPalabra: ";
+
+		cout << help;
 	}
 
 
-	cout<<endl<<endl;
+	cout<< "\n\n";
 	system("pause");
 	return 0;
 }
 
 void show(){
 	cls;
-	cout << "\n\tAHORCADO \n\n\n";
-	printf("\t");
-	for (int i = 0; i< palabra.length();i++) printf("%c",palabra[i]);
-	cout<<endl<<endl;	
+	cout << "\n\tAHORCADO \n\n\n\t";
+
+	cout << palabra << "\n\n";
 }
 
 void play(){
@@ -64,18 +66,16 @@ void play(){
 
 		cout << "\n\tJuega\n";
 		cout << "\n Vidas restantes: "<<life;
-		if (cont >= 1){
-			cout << "\n\n Letras usadas: ";cout<<letrasUsadas;
-		} 
+		if (letrasUsadas.length() >= 1)
+			cout << "\n\n Letras usadas: " << letrasUsadas;
+
 		cout << "\n\n Ingresa una letra: "; cin.getline(key,100,'\n'); 
 
 		//Si ingresa una letra minuscula pasa a mayuscula
-		if 	( key[0] >= 97 || key[0] <= 122) 
-		{
+		if 	( key[0] >= 'a' && key[0] <= 'z') 
 			key[0] -= 32; 
-		}
 	
-		if (strlen(key) >= 2 ){
+		if (strlen(key) > 1 ){
 			cout << "\nOye te he pedido solo una letra. Intentalo de nuevo\n\n";
 			system("pause");
 			show();
@@ -96,8 +96,8 @@ void play(){
 
 	}while(repetido);
 
-	letrasUsadas[cont] = key[0]; //Almacena las letras que se han utilizado
- 	cont++; //controla las veces que se ingresa una letra correcta
+	letrasUsadas += key[0]; //Almacena las letras que se han utilizado
+ 	// cont++; //controla las veces que se ingresa una letra correcta
  		   //Sera la posicion donde se guarde la letra ingresada 
 }
 
@@ -107,7 +107,10 @@ void esconder(){
 
 	for (int i = 0; i <  palabra.length(); i++){
 
-		if(palabra[i] != ' ') palabra[i] = '-'; help[i] -= 32;
+		if(palabra[i] != ' ') {
+			palabra[i] = '-'; 
+			help[i] -= 32;
+		}
 	}	
 }
 
@@ -127,20 +130,16 @@ bool check(){
 	bien = 0;
 
 	//DEVUELVE TRUE SOLO SI HA GANADO O SI HA PERDIDO
-	if (life == 0) return true;
-	else if (help == palabra) return true;
-	else return false;
+	return life == 0 || help == palabra;
 }
 
 //Determinar que la letra ingresada no este repetida
 bool repeat(){
 
-	int asd = 0;
-
-	for(int i = 0; i <= strlen(letrasUsadas); i++) {
-		if (key[0] == letrasUsadas[i]) asd++;
+	for(int i = 0; i <= letrasUsadas.length(); i++) {
+		if (key[0] == letrasUsadas[i]) 
+			return true;
 	}
 
-	if (asd == 0) return false;
-	else return true;
+	return false;
 }
